519403217cf898f9836cb103bc84d2474fd00c51
Jan <jasjago@gmail.com>
2023-10-16 15:07:35 +0200

    Initial Commit

---
+++ Poset/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Poset
+//
+//  Created by Jan Jagodziński on 16/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
